5ccce1ad5f338ca58478422183927e9a9bfbb48d
Abhinav P <abhinav.chek@gmail.com>
2020-12-28 23:31:39 +0530

    test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 window.addEventListener("deviceorientation", handleOrientation, true);
 function handleOrientation(e){
-    document.getElementById('box').setAttribute('rotation',e.alpha+" "+"30 20")
+    document.getElementById('box').setAttribute('rotation',e.gamma+" 30 20")
 }
