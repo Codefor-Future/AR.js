345133b34bb23aacd60395ddc4dca1394c7f9a7d
Abhinav P <abhinav.chek@gmail.com>
2020-12-28 23:51:20 +0530

    earth

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.addEventListener("deviceorientation", handleOrientation, true);
-function handleOrientation(e){
-    document.getElementById('box').setAttribute('rotation',e.gamma+" 30 20")
-}
+// window.addEventListener("deviceorientation", handleOrientation, true);
+// function handleOrientation(e){
+//     document.getElementById('box').setAttribute('rotation',e.gamma+" 30 20")
+// }
